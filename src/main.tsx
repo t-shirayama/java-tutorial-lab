b37460f5c32d72af74b25fda3e68c62f728f32e2
t-shirayama <t.shirayama3@gmail.com>
2026-05-13 23:29:34 +0900

    feat: フォントの追加とスタイルの更新

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,13 @@ import {
   Timer,
   X
 } from "lucide-react";
+import "@fontsource/noto-sans-jp/400.css";
+import "@fontsource/noto-sans-jp/500.css";
+import "@fontsource/noto-sans-jp/700.css";
+import "@fontsource/noto-sans-jp/900.css";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/600.css";
+import "@fontsource/jetbrains-mono/700.css";
 import "highlight.js/styles/github.css";
 import "./styles.css";
 import {
